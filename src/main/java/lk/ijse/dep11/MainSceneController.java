0ff1e47b65752bbb3e2fe45750a4c0bc9d905a9d
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-08-22 00:35:06 +0530

    MainScene UI created and linked with controller

--- src/main/java/lk/ijse/dep11/MainSceneController.java
+++ src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -1,4 +1,25 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public MenuItem menuItmNew;
+    public MenuItem menuItmExit;
+    public MenuItem menuItmUserGuide;
+    public MenuItem menuItmAboutUs;
+
+    public void menuItmNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuItmExit(ActionEvent actionEvent) {
+    }
+
+    public void menuItmUserGuideOnAction(ActionEvent actionEvent) {
+    }
+
+    public void menuItmAboutUsOnAction(ActionEvent actionEvent) {
+    }
 }
